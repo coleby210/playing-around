09b66ac6930c0fcd1d17eddc1d07622f68610fc4
Coleby Kent <coleby210@gmail.com>
2016-01-23 09:01:21 -0800

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
 require "faker"
 
-10.times do
-  you = Member.create({username: Faker::Internet.user_name, password_hash: Faker::Internet.password(8)})
-  you.save
-end
+# 10.times do
+#   you = Member.create({username: Faker::Internet.user_name, password_hash: Faker::Internet.password(8)})
+#   you.save
+# end
 
-Boss.create(name: "Behemoth")
+Boss.create(name: "Mountain Behemoth")
 Boss.create(name: "Demon Servant")
 Boss.create(name: "Ocean Dragon")
+Boss.create(name: "Ogre Master")
+Boss.create(name: "Baphomat")
+Boss.create(name: "Manticore")
+Boss.create(name: "Bone Drake")
+Boss.create(name: "Eragon (Dragon)")
+Boss.create(name: "Minotaur Master")

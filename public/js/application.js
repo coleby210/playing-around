2794374830a08e149231f0bb489bef19c9167714
Coleby Kent <coleby210@gmail.com>
2016-01-31 10:04:24 -0800

    Fix multi-editing on logs

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
   $(".edit-button").click(function(event){
     event.preventDefault();
-    url = $(this).attr("href");
-    $.ajax({
-      method: "GET",
-      url: url
-    }).done(function(response){
-      $("#start").parent().prepend(response);
-    });
+    if ($(".edit-button").hasClass("disabled")){
+      return false
+    }
+    else {
+      url = $(this).attr("href");
+      $.ajax({
+        method: "GET",
+        url: url
+      }).done(function(response){
+        $("#start").parent().prepend(response);
+        $(".edit-button").addClass('disabled');
+      });
+    };
   });
 })
 
